cbd640e071274db976a0c132cf0c81a541943c34
Gianluca Galli <gianluca.galli9519@gmail.com>
2025-09-17 15:20:18 +0100

    Fix build issues: Update image component and escape quotes

--- src/app/components/TrustBadges.tsx
+++ src/app/components/TrustBadges.tsx
@@ -1,11 +1,20 @@
 'use client';
+import Image from 'next/image';
 
 export default function TrustBadges() {
   return (
     <section aria-label="Certifications" className="py-10">
       <div className="max-w-6xl mx-auto px-4">
         <div className="flex items-center justify-center">
-          <img src="/napit.svg" alt="NAPIT Certified" className="h-24 w-auto" />
+          <div className="relative h-24 w-48">
+            <Image 
+              src="/napit.svg" 
+              alt="NAPIT Certified" 
+              fill
+              className="object-contain"
+              priority
+            />
+          </div>
         </div>
       </div>
     </section>
